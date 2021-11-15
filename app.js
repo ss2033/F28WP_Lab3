5cb9b5eb02d337ff175c5a01fe5c34eced47cfe7
ss2033 <ss2033@hw.ac.uk>
2021-11-15 09:29:27 +0400

    Changes

--- app.js
+++ app.js
@@ -3,10 +3,24 @@ const express = require('express');
 //creating app
 const app = express();
 
-//send an HTTP response when receiving HTTP GET /
+//handling static HTML and EJS templates
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
-    res.send("Hello, your server is up and running");
-   });
+    res.render('index'); //no need for ejs extension
+});
+//route for contacts
+app.get('/contacts', (req, res) => {
+    res.render('contacts');
+});
+//route for client registration
+app.get('/api/register', (req, res) => {
+    res.render('api/register');
+});
+//route for client login
+app.get('/api/login', (req, res) => {
+    res.render('api/login');
+});
 
 //make the app listen on port
 const port = process.argv[2] || process.env.PORT || 3000;
